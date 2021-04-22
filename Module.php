68598b80f58dec1e1f88a036638b694df704b474
CyrineLouati <47759860+CyrineLouati@users.noreply.github.com>
2021-04-22 15:11:03 +0200

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Module
+ *
+ * @ORM\Table(name="module")
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\ModulesRepository")
+
+ */
+class Module
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="No file chosen. Please choose File.")
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+}
